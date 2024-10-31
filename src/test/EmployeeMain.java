bf7e8e34a0f083df5518fd2cd405f3995094976d
Uttam Thakare <56421970+uttam20-35@users.noreply.github.com>
2024-10-31 21:42:41 +0530

    added a switch-case to main

--- src/test/EmployeeMain.java
+++ src/test/EmployeeMain.java
@@ -2,17 +2,37 @@ package test;
 
 import impl.EmployeeImpl;
 
+import java.util.Scanner;
+
 public class EmployeeMain {
     public static void main(String[] args) {
 
+        System.out.println("1. List of the Employees");
+        System.out.println("2. Add new Employee");
+        System.out.println("3. Search Employee");
+        System.out.println("4. Delete Employee");
+        System.out.println("5. Update Employee");
+        System.out.println("Select above Numbers for the operations :");
+
+        Scanner sc =new Scanner(System.in);
+        int number= sc.nextInt();
+
         EmployeeImpl employee =new EmployeeImpl();
 
-        employee.getAllEmployees();
-        employee.addEmployee();
-        employee.getEmployeeById();
-        employee.deleteEmployee();
-        //both methods required for update in table
-        employee.preloadData();
-        employee.updateEmployeeById();
+        switch (number){
+
+            case 1: employee.getAllEmployees();
+            break;
+            case 2: employee.addEmployee();
+            break;
+            case 3: employee.getEmployeeById();
+            break;
+            case 4: employee.deleteEmployee();
+            break;
+            case 5: employee.updateEmployeeById();
+            break;
+            default:
+                System.out.println("Wrong input");
+        }
     }
 }
